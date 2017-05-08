07be9f7cbedc2dab31434f048da7566cb61d0076
jenzhao <jennifer.zhao.15@gmail.com>
2017-05-08 13:50:16 -0700

    modify the w2vstring transfrom testing case

--- tests/test_w2vString_transform.py
+++ tests/test_w2vString_transform.py
@@ -1,25 +1,22 @@
 import unittest
 import pandas as pd
-import numpy as np
 
 from hidi.factorization import W2VStringTransform
 
 
 class TestW2VStringTransform(unittest.TestCase):
     def setUp(self):
-        self.df = pd.DataFrame(np.random.randint(
-            0, 50, size=(1000, 2)),
-            columns=['item_id', 'link_id'])
-        self.w2vstring = W2VStringTransform(get_item_embeddings=False)
-        self.string, _ = self.w2vstring.transform(self.df)
+        self.x = ['vevo1', 'vevo2', 'vevo3', 'vevo4',  'vevo5']
+        self.y = [1, 1, 1, 2, 3]
+        self.df = pd.DataFrame({'link_id': self.y,
+                                'item_id': self.x})
+        self.w2vstring = W2VStringTransform()
+        self.sentences, _ = self.w2vstring.transform(self.df)
 
     def test_w2vstring_returns_string(self):
-        self.assertEqual(type("string"), type(self.string))
+        self.assertEqual(type([]), type(self.sentences))
 
-    def test_w2vstring_returns_1000X3_item(self):
-        df = self.df
-        word_list = self.string.split()
-        total_item = 0
-        for index in df.index.unique():
-            total_item = len(df.loc[index].item_id)*3 + total_item
-        self.assertEqual(total_item, len(word_list))
+    def test_w2vstring_returns_lenX3_item(self):
+        length = sum([len(i) for i in self.sentences])
+        self.assertEqual(len(set(self.y)), len(self.sentences))
+        self.assertEqual(len(self.x)*3, length)
